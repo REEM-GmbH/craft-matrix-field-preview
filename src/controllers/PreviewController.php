525be8a7fa35c9c260d5f88dc93f41680de5b30c
d.tesch <d.tesch@reem.at>
2025-07-17 21:04:56 +0200

    [int|Craft|LR25001074] Craft 5 Upgrade

--- src/controllers/PreviewController.php
+++ src/controllers/PreviewController.php
@@ -8,6 +8,8 @@ use craft\web\Controller;
 use weareferal\matrixfieldpreview\MatrixFieldPreview;
 use yii\helpers\Markdown;
 
+use vaersaagod\matrixmate\MatrixMate;
+
 /**
  * Preview controller
  *
@@ -15,129 +17,166 @@ use yii\helpers\Markdown;
  */
 class PreviewController extends Controller
 {
-
-    protected array|bool|int $allowAnonymous = [];
-
-    /**
-     * Get preview config
-     *
-     * Return a JSON configuration for the frontend to use
-     *
-     * NOTE: there are two "handles" in play here: the matrix field handle
-     * as well as the block type handles
-     */
-    public function actionGetPreviews($type, $fieldHandle)
-    {
-        $plugin = MatrixFieldPreview::getInstance();
-        $settings = $plugin->getSettings();
-        $response = [
-            "success" => false,
-            "config" => [
-                "field" => null,
-                "blockTypes" => [],
-                "categories" => []
-            ],
-        ];
-
-        switch ($type) {
-            case "matrix":
-                $fieldService = $plugin->matrixFieldConfigService;
-                $blockTypeService = $plugin->matrixBlockTypeConfigService;
-                break;
-            case "neo":
-                $fieldService = $plugin->neoFieldConfigService;
-                $blockTypeService = $plugin->neoBlockTypeConfigService;
-                break;
-            default:
-                $response["error"] = "'type' must be 'matrix' or 'neo'";
-                return $this->asJson($response);
-        }
-
-        $fieldConfig = $fieldService->getOrCreateByFieldHandle($fieldHandle);
-
-        if (!$fieldConfig) {
-            return $this->asJson($response);
-        }
-
-        // Add field info
-        $response['config']['field'] = [
-            "name" => $fieldConfig->field->name,
-            "handle" => $fieldConfig->field->handle,
-            "enablePreviews" => (bool)$fieldConfig->enablePreviews,
-            "enableTakeover" => (bool)$fieldConfig->enableTakeover,
-            "buttonLabel" => $fieldConfig->buttonLabel,
-            "buttonIcon" => "",
-            "buttonIconSvg" => "",
-        ];
-
-        // Add button icon SVG if configured
-        if ($fieldConfig->buttonIcon && $fieldConfig->buttonIcon !== "") {
-            $response['config']['field']['buttonIcon'] = $fieldConfig->buttonIcon;
-            $response['config']['field']['buttonIconSvg'] = Cp::iconSvg($fieldConfig->buttonIcon);
-        }
-
-        // Add categories
-        foreach ($plugin->categoryService->getAll() as $category) {
-            array_push($response['config']["categories"], [
-                'id' => $category->id,
-                'name' => $category->name,
-                'description' => $category->description,
-                "descriptionHTML" => Markdown::process($category->description),
-            ]);
-        }
-
-        // Add block type preview info
-        $blockTypeConfigs = $blockTypeService->getOrCreateByFieldHandle($fieldHandle);
-        foreach ($blockTypeConfigs as $blockTypeConfig) {
-            $blockType = $blockTypeConfig->blockType;
-            // Skip block types that are not enabled
-            if (! (bool)$blockTypeConfig->enabled) {
-                continue;
-            }
-            // Skip Neo block types that are not enabled
-            if ($type == "neo" && $blockType->hasAttribute('enabled') && ! (bool)$blockType->enabled) {
-                continue;
-            }
-            $result = [
-                "name" => $blockType->name,
-                "handle" => $blockType->handle,
-                "description" => $blockTypeConfig->description,
-                "descriptionHTML" => Markdown::process($blockTypeConfig->description),
-                "categoryId" => $blockTypeConfig->categoryId,
-                "image" => null,
-                "thumb" => null,
-                "blockTypeId" => $blockType->id
-            ];
-            if ($blockTypeConfig->previewImageId) {
-                $asset = Craft::$app->assets->getAssetById($blockTypeConfig->previewImageId);
-                $result["imageId"] = $blockTypeConfig->previewImageId;
-                if ($asset->extension == "gif" && Craft::$app->config->general->transformGifs == false) {
-                    $result["image"] = $asset ? $asset->getUrl() : "";
-                    $result["thumb"] = $asset ? $asset->getUrl() : "";
-                } else {
-                    $result["image"] = $asset ? $asset->getUrl([
-                        "width" => 1600,
-                        "mode" => "fit",
-                        "position" => "center-center",
-                        "quality" => 99
-                    ]) : "";
-                    $result["thumb"] = $asset ? Craft::$app->assets->getThumbUrl($asset, 600, 600) : "";
-                }
-
-
-            }
-            $response['config']["blockTypes"][$blockType->handle] = $result;
-        }
-
-        // Add neo-specific setting
-        if ($type == "neo") {
-            $response["config"]["neo"] = [
-                "neoDisableForSingleChilden" => $settings->neoDisableForSingleChilden
-            ];
-        }
-
-        $response["success"] = true;
-
-        return $this->asJson($response);
-    }
+	
+	protected array|bool|int $allowAnonymous = [];
+	
+	/**
+	 * Get preview config
+	 *
+	 * Return a JSON configuration for the frontend to use
+	 *
+	 * NOTE: there are two "handles" in play here: the matrix field handle
+	 * as well as the block type handles
+	 */
+	public function actionGetPreviews($type, $fieldHandle)
+	{
+		$plugin = MatrixFieldPreview::getInstance();
+		$settings = $plugin->getSettings();
+		$response = [
+			"success" => false,
+			"config"  => [
+				"field"      => null,
+				"blockTypes" => [],
+				"categories" => [],
+			],
+		];
+		
+		switch ($type) {
+			case "matrix":
+				$fieldService = $plugin->matrixFieldConfigService;
+				$blockTypeService = $plugin->matrixBlockTypeConfigService;
+				break;
+			case "neo":
+				$fieldService = $plugin->neoFieldConfigService;
+				$blockTypeService = $plugin->neoBlockTypeConfigService;
+				break;
+			default:
+				$response["error"] = "'type' must be 'matrix' or 'neo'";
+				
+				return $this->asJson($response);
+		}
+		
+		$fieldConfig = $fieldService->getOrCreateByFieldHandle($fieldHandle);
+		
+		if ( ! $fieldConfig) {
+			return $this->asJson($response);
+		}
+		
+		// Add field info
+		$response['config']['field'] = [
+			"name"           => $fieldConfig->field->name,
+			"handle"         => $fieldConfig->field->handle,
+			"enablePreviews" => (bool)$fieldConfig->enablePreviews,
+			"enableTakeover" => (bool)$fieldConfig->enableTakeover,
+			"buttonLabel"    => $fieldConfig->buttonLabel,
+			"buttonIcon"     => "",
+			"buttonIconSvg"  => "",
+		];
+		
+		// Add button icon SVG if configured
+		if ($fieldConfig->buttonIcon && $fieldConfig->buttonIcon !== "") {
+			$response['config']['field']['buttonIcon'] = $fieldConfig->buttonIcon;
+			$response['config']['field']['buttonIconSvg'] = Cp::iconSvg($fieldConfig->buttonIcon);
+		}
+		
+		// Add categories
+		foreach ($plugin->categoryService->getAll() as $category) {
+			array_push($response['config']["categories"], [
+				'id'              => $category->id,
+				'name'            => $category->name,
+				'description'     => $category->description,
+				"descriptionHTML" => Markdown::process($category->description),
+			]);
+		}
+		
+		// Add block type preview info
+		$blockTypeConfigs = $blockTypeService->getOrCreateByFieldHandle($fieldHandle);
+		foreach ($blockTypeConfigs as $blockTypeConfig) {
+			$blockType = $blockTypeConfig->blockType;
+			
+			if (Craft::$app->plugins->isPluginInstalled('matrixmate')) {
+				$matrixmate = MatrixMate::getInstance()->getSettings();
+				$entryId = intval(basename(parse_url(Craft::$app->request->getReferrer(), PHP_URL_PATH)));
+				$entry = Craft::$app->entries->getEntryById($entryId, Craft::$app->sites->getAllSiteIds(), ['status' => null]);
+				
+				if ($entry) {
+					$sectionHandle = $entry->getSection()->handle;
+					$field = Craft::$app->getFields()->getFieldById($blockTypeConfig->fieldId);
+					$entryTypes = $field->getSettings()['entryTypes'] ?? [];
+					
+					$fieldUid2blockTypeUid = null;
+					$fieldHandle2blockTypeHandle = null;
+					
+					foreach ($entryTypes as $entryType) {
+						if (isset($entryType['handle']) && $entryType['uid'] === $blockType->uid) {
+							$fieldUid2blockTypeUid = $entryType['uid'];
+							$fieldHandle2blockTypeHandle = $entryType['handle'];
+							break;
+						}
+					}
+					
+					$fieldConfig = $matrixmate->fields[$response['config']['field']['handle']] ?? [];
+					foreach ($fieldConfig as $key => $value) {
+						$sectionKey = explode(':', $key)[1] ?? null;
+						if ($sectionKey === $sectionHandle) {
+							foreach ($value['hiddenTypes'] as $hiddenType) {
+								if ($hiddenType === $blockType->handle
+								    || ($blockType->uid === $fieldUid2blockTypeUid && $fieldHandle2blockTypeHandle === $hiddenType)
+								) {
+									continue 3; // Überspringt die äußere Schleife
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			// Skip block types that are not enabled
+			if ( ! (bool)$blockTypeConfig->enabled) {
+				continue;
+			}
+			// Skip Neo block types that are not enabled
+			if ($type == "neo" && $blockType->hasAttribute('enabled') && ! (bool)$blockType->enabled) {
+				continue;
+			}
+			$result = [
+				"name"            => $blockType->name,
+				"handle"          => $blockType->handle,
+				"description"     => $blockTypeConfig->description,
+				"descriptionHTML" => Markdown::process($blockTypeConfig->description),
+				"categoryId"      => $blockTypeConfig->categoryId,
+				"image"           => null,
+				"thumb"           => null,
+				"blockTypeId"     => $blockType->id,
+			];
+			if ($blockTypeConfig->previewImageId) {
+				$asset = Craft::$app->assets->getAssetById($blockTypeConfig->previewImageId);
+				$result["imageId"] = $blockTypeConfig->previewImageId;
+				if ($asset->extension == "gif" && Craft::$app->config->general->transformGifs == false) {
+					$result["image"] = $asset ? $asset->getUrl() : "";
+					$result["thumb"] = $asset ? $asset->getUrl() : "";
+				} else {
+					$result["image"] = $asset ? $asset->getUrl([
+						"width"    => 1600,
+						"mode"     => "fit",
+						"position" => "center-center",
+						"quality"  => 99,
+					]) : "";
+					$result["thumb"] = $asset ? Craft::$app->assets->getThumbUrl($asset, 600, 600) : "";
+				}
+			}
+			$response['config']["blockTypes"][$blockType->handle] = $result;
+		}
+		
+		// Add neo-specific setting
+		if ($type == "neo") {
+			$response["config"]["neo"] = [
+				"neoDisableForSingleChilden" => $settings->neoDisableForSingleChilden,
+			];
+		}
+		
+		$response["success"] = true;
+		
+		return $this->asJson($response);
+	}
 }
